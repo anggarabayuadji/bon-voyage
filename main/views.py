3f7ff01d02354c22483b33676cc7658e148eed2c
angelobenhanan <angelo.benhanan@ui.ac.id>
2025-07-09 10:20:44 +0700

    made beautifulsoup to comment to check for errors

--- main/views.py
+++ main/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponseRedirect,HttpResponse
 from bs4 import BeautifulSoup
 
 # Mencari lokasi yang dituju di Google
-find = input()
-url = f"https://www.google.com/search?q={find}"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
-mysearch = soup.find
-print(mysearch.text)
+# find = input()
+# url = f"https://www.google.com/search?q={find}"
+# req = requests.get(url)
+# soup = BeautifulSoup(req.text, "html.parser")
+# mysearch = soup.find
+# print(mysearch.text)
 
 # Create your views here.
